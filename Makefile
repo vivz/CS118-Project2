c619b16b5498f5af20f0600670a76f863c53dad9
Simon Zou <simon.zou@gmail.com>
2016-03-06 20:51:55 -0800

    removed project 1 code and renamed files

--- Makefile
+++ Makefile
@@ -1,6 +1,11 @@
 all:
-    gcc -w -o server server.c
-    gcc -w -o client client.c
+	gcc -w -o sender sender.c
+	gcc -w -o receiver receiver.c
+
+warning:
+	gcc -o server server.c
+	gcc -o receiver receiver.c
 
 clean:
-    rm server
+	rm sender
+	rm receiver
---
+++ sender.c
@@ -0,0 +1,62 @@
+/* A simple server in the internet domain using TCP
+   The port number is passed as an argument 
+   This version runs forever, forking off a separate 
+   process for each connection
+*/
+#include <stdio.h>
+#include <sys/types.h>   // definitions of a number of data types used in socket.h and netinet/in.h
+#include <sys/socket.h>  // definitions of structures needed for sockets, e.g. sockaddr
+#include <netinet/in.h>  // constants and structures needed for internet domain addresses, e.g. sockaddr_in
+#include <stdlib.h>
+#include <strings.h>
+#include <sys/wait.h>   /* for the waitpid() system call */
+#include <signal.h> /* signal name macros, and the kill() prototype */
+
+
+void error(char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+int main(int argc, char *argv[])
+{
+  int sockfd, newsockfd, portno, pid;
+  socklen_t clilen;
+  struct sockaddr_in serv_addr, cli_addr;
+
+  if (argc < 2) {
+     fprintf(stderr,"ERROR, no port provided\n");
+     exit(1);
+  }
+
+  sockfd = socket(AF_INET, SOCK_DGRAM, 0); //create socket
+  if (sockfd < 0) 
+    error("ERROR opening socket");
+  memset((char *) &serv_addr, 0, sizeof(serv_addr));    //reset memory
+  //fill in address info
+  portno = atoi(argv[1]);
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  serv_addr.sin_port = htons(portno);
+
+  if (bind(sockfd, (struct sockaddr *) &serv_addr,
+          sizeof(serv_addr)) < 0) 
+          error("ERROR on binding");
+
+  long file_size;
+
+  int output_buffer_size = 100;
+  char output_buffer[output_buffer_size];
+  int input_buffer_size = 1024;
+
+  char file_name[100];
+  char file_extension[10];
+
+  listen(sockfd,5);  //5 simultaneous connection at most
+
+  close(sockfd);
+     
+  return 0; 
+}
+
