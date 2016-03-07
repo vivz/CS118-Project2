27865b4b1af4aaf66bc37f6ee394c0cf9c230348
vivz <vivian-keke@hotmail.com>
2016-03-06 19:05:58 -0800

    initial

---
+++ server.c
@@ -0,0 +1,13 @@
+
+#include <sys/socket.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+
+
+int main(int argc, char *argv[]) {
+
+	return 0
+}
